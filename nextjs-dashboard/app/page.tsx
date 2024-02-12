3f007803fcd65ae07db7fb5473002e29e9d24d01
Matthew Moyer <moyer0729@uwlax.edu>
2024-02-12 15:42:16 -0600

    Created a basic home page, added prop functionality to button.tsx to accept href, login button on homepage does not work yet

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,23 +1,30 @@
-import AcmeLogo from '@/app/ui/FrontashLogo';
-import LoginForm from '@/app/ui/login';
+import { IoFastFood } from "react-icons/io5";
 import { Button } from '@/app/ui/button';
-  
- 
+
 export default function LoginPage() {
   return (
-    <main className="flex items-center justify-center md:h-screen bg-orange-500">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
-            <AcmeLogo />
-          </div>
+    <main className="flex items-center justify-center md:h-screen bg-blue-700">
+      <div className="relative mx-auto flex w-full flex-col space-y-2.5 p-4 md:-mt-32">
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '180px', fontSize: '120px', textAlign: 'center' }}>
+          <IoFastFood className="h-25 w-25" />
+          <span style={{ marginLeft: '10px' }}>FrontDash</span>
         </div>
-        <LoginForm />
-        <div className="flex h-8 items-end space-x-1">
-          <Button className="w-full">New User? Create Account</Button>
-          </div>
+        <p style={{ textAlign: 'center', fontSize: '1.5rem' }}>
+          FrontDash is a food delivery 
+          service connecting customers with a wide array of local restaurants and eateries. With its user-friendly 
+          platform, customers can easily browse menus, place orders, and receive their food without leaving the comfort of their home. 
+          Offering an extensive selection of cuisines, DoorDash provides convenience and quality, making it a go-to choice for food 
+          enthusiasts seeking diverse dining experiences delivered straight to their doorstep.
+        </p>
+        <div className="flex h-8 items-center justify-center space-x-1">
+          <Button href='/loginPage'>Login</Button>
+          <Button>Register</Button>
+        </div>
       </div>
-      <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-b from-white to-transparent" />
+      
+      <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-b from-black to-transparent flex items-center justify-center">
+        <p>Created by: Matthew Moyer, Tanner Turba, and Samuel Evanson</p>
+      </div>
     </main>
   );
 }
--- nextjs-dashboard/app/ui/button.tsx
+++ nextjs-dashboard/app/ui/button.tsx
@@ -1,10 +1,28 @@
 import clsx from 'clsx';
+import Link from 'next/link'; 
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  href?: string;
 }
 
-export function Button({ children, className, ...rest }: ButtonProps) {
+export function Button({ children, className, href, ...rest }: ButtonProps) {
+  if (href) {
+    return (
+      <Link href={href}>
+        <button
+          {...rest}
+          className={clsx(
+            'flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
+            className,
+          )}
+        >
+          {children}
+        </button>
+      </Link>
+    );
+  }
+  
   return (
     <button
       {...rest}
