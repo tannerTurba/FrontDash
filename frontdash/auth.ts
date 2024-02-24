06918f9b6cf54381e3a25c4fe72c21616bf72c3b
Tanner Turba <turba7166@uwlax.edu>
2024-02-23 21:33:16 -0600

    role specific nav
    Signed-off-by: Tanner Turba <turba7166@uwlax.edu>

--- frontdash/auth.ts
+++ frontdash/auth.ts
@@ -6,6 +6,7 @@ import bcrypt from 'bcrypt';
 import { z } from 'zod';
 import { PrismaClient, User } from '@prisma/client';
 import { authConfig } from './auth.config';
+import { cookies } from 'next/headers';
 
 const prisma = new PrismaClient();
 
@@ -14,13 +15,42 @@ export async function getUser(username: string): Promise<User | null> {
     where: {
         username: username,
     },
-});
-console.log(JSON.stringify(user));
-return user as User;
+  });
+  console.log(JSON.stringify(user));
+  return user as User;
+}
+
+export async function getUserRole() {
+  return await getRole(cookies().get('username').value);
+}
+
+async function getRole(username: string): Promise<string | null> {
+  let user;
+  try {
+    user = await prisma.$queryRaw`SELECT Role.title
+                                    FROM User JOIN WorksAs JOIN Role 
+                                      ON User.id = WorksAs.userId
+                                        AND Role.id = WorksAs.roleId
+                                    WHERE User.username = ${username}`;
+  }
+  catch (error) {
+    console.error('Error executing raw query:', error);
+  }
+  finally {
+    await prisma.$disconnect();
+  }
+  if (user) {
+    return user[0].title;
+  }
 }
  
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  session: {
+    strategy:'jwt',
+    maxAge: 24 * 60 * 60, // 1 day
+  },
+  // adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
         credentials: {
@@ -37,7 +67,14 @@ export const { auth, signIn, signOut } = NextAuth({
             console.log(`Username: ${username} \nPassword: ${password}`);
             let user = await getUser(username);
             if (!user) {
-                return Promise.resolve(null);
+              let userData = {
+                name: username,
+                email: await getRole(username),
+                id: user.id,
+                picture: null,
+              };
+              return userData;
+              // return Promise.resolve(userData);
             }
             
             const passwordsMatch = await bcrypt.compare(password, user.password as string);
