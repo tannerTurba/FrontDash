c241b480a822a37812cd3b3d160ac7dd5d0720ec
Tanner Turba <turba7166@uwlax.edu>
2024-03-10 22:24:55 -0500

    refactor began
    Signed-off-by: Tanner Turba <turba7166@uwlax.edu>

--- frontdash/app/page.tsx
+++ frontdash/app/page.tsx
@@ -1,37 +1,50 @@
-import FrontDashLogo from '@/app/ui/FrontashLogo';
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import Link from 'next/link';
+// import CardWrapper from '@/app/ui/dashboard/cards';
+// import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+// import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+// import { lusitana } from '@/app/ui/fonts';
+// import { Suspense } from 'react';
+// import {
+//   RevenueChartSkeleton,
+//   LatestInvoicesSkeleton,
+//   CardsSkeleton,
+// } from '@/app/ui/skeletons';
 
-export default function Home() {
+import Card from '@/app/ui/dashboard/restaurant-card';
+import GridList from '@/app/ui/dashboard/gridlist';
+
+export default async function Page() {
+  const cards = [
+    { 
+        title: 'Card 1', 
+        description: 'This is the description of Card 1.', 
+        imageUrl: 'https://example.com/image1.jpg' 
+    },
+    { 
+        title: 'Card 2', 
+        description: 'This is the description of Card 2.', 
+        imageUrl: 'https://example.com/image2.jpg' 
+    },
+    { 
+        title: 'Card 3', 
+        description: 'This is the description of Card 3.', 
+        imageUrl: 'https://example.com/image3.jpg' 
+    },
+    { 
+        title: 'Card 4', 
+        description: 'This is the description of Card 4.', 
+        imageUrl: 'https://example.com/image4.jpg' 
+    },
+    { 
+      title: 'Card 5', 
+      description: 'This is the description of Card 5.', 
+      imageUrl: 'https://example.com/image5.jpg' 
+    }
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <FrontDashLogo />
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0">
-        <p style={{ textAlign: 'center', fontSize: '1.5rem' }}>
-          FrontDash is a food delivery 
-          service connecting customers with a wide array of local restaurants and eateries. With its user-friendly 
-          platform, customers can easily browse menus, place orders, and receive their food without leaving the comfort of their home. 
-          Offering an extensive selection of cuisines, DoorDash provides convenience and quality, making it a go-to choice for food 
-          enthusiasts seeking diverse dining experiences delivered straight to their doorstep.
-        </p>
-      </div>
-      <div className="flex h-8 items-center justify-center space-x-1">
-        <Link
-          href="/login"
-          className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-        >
-          <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-        </Link>
-        <Link
-          href="/"
-          className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-        >
-          <span>Register</span> <ArrowRightIcon className="w-5 md:w-6" />
-        </Link>
-      </div>
-      <div className="absolute bottom-0 left-0 right-0 h-32 flex items-center justify-center">
-        <p>Created by: Matthew Moyer, Tanner Turba, and Samuel Evanson</p>
+    <main>
+      <div className="grow p-6 md:overflow-y-auto md:p-12">
+          <GridList cards={cards} />
       </div>
     </main>
   );
