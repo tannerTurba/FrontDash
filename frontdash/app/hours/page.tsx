ede9b4cf36165b48823cebe08297a33353e74a47
Tanner Turba <turba7166@uwlax.edu>
2024-03-11 17:28:22 -0500

    validations
    Signed-off-by: Tanner Turba <turba7166@uwlax.edu>

--- frontdash/app/hours/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Page() {
-    return (
-      <main>
-        <h1>Hours</h1>
-      </main>
-    );
-  }
--- frontdash/scripts/runner.ts
+++
@@ -1,6 +0,0 @@
-import { createUser } from './account'
-
-/** To run, compile this file and any related files using 'tsc <filename>'.
- * Then run the file by calling 'node runner.js'
- */
-// createUser('employee', '123456');
