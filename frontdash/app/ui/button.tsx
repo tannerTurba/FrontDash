2213e06f86bdcf7e882a418a90d2b3e1620cc87f
Tanner Turba <turba7166@uwlax.edu>
2024-03-30 19:02:10 -0500

    withdraw started
    Signed-off-by: Tanner Turba <turba7166@uwlax.edu>

--- frontdash/app/ui/button.tsx
+++ frontdash/app/ui/button.tsx
@@ -1,12 +1,14 @@
 import clsx from 'clsx';
 import Link from 'next/link'; 
+import { MouseEventHandler } from 'react';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   href?: string;
+  action?: MouseEventHandler<HTMLButtonElement>;
 }
 
-export function Button({ children, className, href, ...rest }: ButtonProps) {
+export function Button({ children, className, href, action, ...rest }: ButtonProps) {
   if (href) {
     return (
       <Link href={href}>
@@ -20,6 +22,20 @@ export function Button({ children, className, href, ...rest }: ButtonProps) {
           {children}
         </button>
       </Link>
+    );
+  }
+  else if (action) {
+    return (
+      <button
+      {...rest}
+      className={clsx(
+        'flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
+        className,
+      )}
+      onClick={action}
+    >
+      {children}
+    </button>
     );
   }
   
--- frontdash/scripts/business.ts
+++ frontdash/scripts/business.ts
@@ -1,4 +1,5 @@
 import { PrismaClient, Business } from '@prisma/client';
+import exp from 'constants';
 
 export async function insertBusinessReachedAt(businessId: string, contactId: string) {
     const prisma = new PrismaClient();
@@ -40,6 +41,40 @@ export async function getAllRestaurants(): Promise<Business[]> {
         console.error('Error fetching restaurants:', error);
         return [];
     } finally {
+        await prisma.$disconnect();
+    }
+}
+
+export async function withdraw(username: string) {
+    const prisma = new PrismaClient();
+    try {
+        await prisma.$queryRaw`UPDATE User AS u JOIN WorksFor AS w ON u.id = w.userId
+                JOIN Business AS b ON b.id = w.businessId
+            SET u.status = 'inactive',
+                b.status = 'inactive'
+            WHERE u.username = ${username}`;
+    }
+    catch (error) {
+        console.error('Error executing raw query(insertBusinessReachedAt):', error);
+    }
+    finally {
+        await prisma.$disconnect();
+    }
+}
+
+export async function reactivate(business: string) {
+    const prisma = new PrismaClient();
+    try {
+        await prisma.$queryRaw`UPDATE User AS u JOIN WorksFor AS w ON u.id = w.userId
+                JOIN Business AS b ON b.id = w.businessId
+            SET u.status = 'active',
+                b.status = 'active'
+            WHERE b.name = ${business}`;
+    }
+    catch (error) {
+        console.error('Error executing raw query(insertBusinessReachedAt):', error);
+    }
+    finally {
         await prisma.$disconnect();
     }
 }
