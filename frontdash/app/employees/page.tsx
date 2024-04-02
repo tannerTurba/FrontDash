898a04b59c8fe9af948460d6e755ea40eb8932b1
Samuel Evanson <evanson3437@uwlax.edu>
2024-04-01 21:32:01 -0500

    alter add employee button

--- frontdash/app/employees/page.tsx
+++ frontdash/app/employees/page.tsx
@@ -1,12 +1,14 @@
-'use client'
 import EmployeePage from '@/app/ui/employeePage'; 
+import Buttons from "./button-container";
 
 export default async function Page() {
   return (
     <main>
+      <Buttons />
       <EmployeePage /> 
     </main>
   );
 }
+
 
 
