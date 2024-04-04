c18343e136876d292e7c57aeac0441fdb37cf4fb
Tanner Turba <turba7166@uwlax.edu>
2024-04-04 00:20:48 -0500

    missed a change
    Signed-off-by: Tanner Turba <turba7166@uwlax.edu>

--- frontdash/app/orders/page.tsx
+++ frontdash/app/orders/page.tsx
@@ -1,9 +1,11 @@
 import OrderList from '@/app/ui/orderScreen';
+import Orderscreen from '@/app/ui/orderScreen'
 
 function OrdersPage() {
   return (
     <div>
       <OrderList />
+      <Orderscreen />
     </div>
   );
 }
