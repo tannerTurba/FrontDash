b7c9f449f6b14199691aa004f8805a4b82f598b1
Matthew Moyer <moyer0729@uwlax.edu>
2024-04-14 17:53:49 -0500

    Added separate menu page for employees/managers --Signing out from page doesn't route back to dashboard correctly

---
+++ frontdash/app/business/page.tsx
@@ -0,0 +1,130 @@
+'use client';
+
+import { ShoppingCartIcon } from '@heroicons/react/24/outline';
+import { useSearchParams } from 'next/navigation';
+import { useState, useRef } from 'react';
+
+export default async function Page() {
+  const searchParams = useSearchParams();
+  const restaurantId = searchParams.get('id');
+
+  try {
+    const res = await fetch(`http://localhost:3000/api/restaurants/${restaurantId}`, {
+      method: 'GET',
+      headers: { id: restaurantId }
+    });
+    const restaurantInfo = await res.json();
+
+    const contact = restaurantInfo.contactInfo[0];
+    const hours = restaurantInfo.availability[0];
+
+    return (
+      <main>
+        <div className="container mx-auto py-8">
+          <div className="max-w-4xl mx-auto">
+            <div className="text-center mb-8">
+              <h1 className="text-3xl font-bold">{restaurantInfo.restaurant?.name}</h1>
+              <p className="text-gray-600">{restaurantInfo.restaurant?.description}</p>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div>
+              <h2 className="text-xl font-semibold mb-2">Hours</h2>
+              <p className="text-gray-700">Monday: {hours?.monOpen.toLocaleString([], {hour: '2-digit', minute:'2-digit'})} - {hours?.monClose.toLocaleString([], {hour: '2-digit', minute:'2-digit'})}</p>
+              <p className="text-gray-700">Tuesday: {hours?.tuesOpen.toLocaleString([], {hour: '2-digit', minute:'2-digit'})} - {hours?.tuesClose.toLocaleString([], {hour: '2-digit', minute:'2-digit'})}</p>
+              <p className="text-gray-700">Wednesday: {hours?.wedOpen.toLocaleString([], {hour: '2-digit', minute:'2-digit'})} - {hours?.wedClose.toLocaleString([], {hour: '2-digit', minute:'2-digit'})}</p>
+              <p className="text-gray-700">Thursday: {hours?.thurOpen.toLocaleString([], {hour: '2-digit', minute:'2-digit'})} - {hours?.thurClose.toLocaleString([], {hour: '2-digit', minute:'2-digit'})}</p>
+              <p className="text-gray-700">Friday: {hours?.friOpen.toLocaleString([], {hour: '2-digit', minute:'2-digit'})} - {hours?.friClose.toLocaleString([], {hour: '2-digit', minute:'2-digit'})}</p>
+              <p className="text-gray-700">Saturday: {hours?.satOpen.toLocaleString([], {hour: '2-digit', minute:'2-digit'})} - {hours?.satClose.toLocaleString([], {hour: '2-digit', minute:'2-digit'})}</p>
+              <p className="text-gray-700">Sunday: {hours?.sunOpen.toLocaleString([], {hour: '2-digit', minute:'2-digit'})} - {hours?.sunClose.toLocaleString([], {hour: '2-digit', minute:'2-digit'})}</p>
+            </div>
+              <div>
+                <h2 className="text-xl font-semibold mb-2">Contact Information</h2>
+                <p className="text-gray-700">Name: {contact?.firstName} {contact?.lastName}</p>
+                <p className="text-gray-700">Address: {contact?.buildingNumber} {contact?.street} {contact?.unitNumber}, {contact?.city}, {contact?.state} {contact?.zipCode}</p>
+                <p className="text-gray-700">Phone Number: {contact?.phoneNumber}</p>
+              </div>
+            </div>
+            <div className="mt-8">
+              <h2 className="text-xl font-semibold mb-2">Menu</h2>
+                <div className="bg-white rounded-lg shadow-md p-4">
+                {restaurantInfo.menuItems.map((item, index) => (
+                    <MenuItem key={index} item={item} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="fixed bottom-4 right-4">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center">
+            <ShoppingCartIcon className="h-6 w-6 mr-2" />
+            Shopping Cart
+            <div className="absolute top-0 right-0 bg-red-500 rounded-full h-6 w-6 flex items-center justify-center text-white text-xs -mt-2 -mr-2">
+              5
+            </div>
+          </button>
+        </div>
+      </main>
+    );
+   } catch (error) {
+    console.error('Error fetching data:', error);
+    return (
+      <main>
+        <div className="container mx-auto py-8">
+          <div className="max-w-4xl mx-auto">
+            <p className="text-red-600">Error fetching data. Please try again later.</p>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+function MenuItem({ item }) {
+  const [quantity, setQuantity] = useState(0);
+  const inputRef = useRef(null);
+
+  const incrementQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decrementQuantity = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    if (value === "" || (Number.isInteger(parseInt(value)) && parseInt(value) >= 0)) {
+      setQuantity(value === "" ? 0 : parseInt(value));
+      inputRef.current.style.width = `${inputRef.current.scrollWidth}px`;
+    }
+  };
+
+  return (
+    <div className="relative flex justify-between items-center bg-gray-200 mb-2 p-2 rounded-lg hover:bg-sky-100 hover:text-blue-600 dark:hover:bg-sky-900 dark:hover:text-blue-400">
+      <div>
+        <p className="text-gray-700 dark:text-gray-300">{item.name}</p>
+        <p className="text-gray-700 dark:text-gray-300">${item.price.toFixed(2)}</p>
+        <p className="text-gray-700 dark:text-gray-300">Available: {item.stock}</p>
+      </div>
+      <div className="flex items-center justify-end">
+        <button className="flex-shrink-0 bg-red-500 hover:bg-red-700 text-white font-bold rounded-full flex items-center justify-center w-8 h-8 text-lg mr-2"
+        onClick={decrementQuantity}>
+          -
+        </button>
+        <input
+          className="text-lg mr-2 w-8 text-center"
+          type="text"
+          value={quantity}
+          onChange={handleChange}
+          ref={inputRef}
+        />
+        <button className="flex-shrink-0 bg-red-500 hover:bg-red-700 text-white font-bold rounded-full flex items-center justify-center w-8 h-8 text-lg mr-5"
+        onClick={incrementQuantity}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
--- frontdash/app/menu/page.tsx
+++ frontdash/app/menu/page.tsx
@@ -1,76 +1,23 @@
-'use client';
-
-import { ShoppingCartIcon } from '@heroicons/react/24/outline';
-import { useSearchParams } from 'next/navigation';
-import { useState, useRef } from 'react';
+import { cookies } from "next/headers";
+import { getFoodItemsByBusiness, getBusinessId } from "@/scripts/business";
 
 export default async function Page() {
-  const searchParams = useSearchParams();
-  const restaurantId = searchParams.get('id');
-  const value = searchParams.get('bool');
-
   try {
-    const res = await fetch(`http://localhost:3000/api/restaurants/${restaurantId}`, {
-      method: 'GET',
-      headers: { id: restaurantId }
-    });
-    const restaurantInfo = await res.json();
-
-    const contact = restaurantInfo.contactInfo[0];
-    const hours = restaurantInfo.availability[0];
+    let username = cookies().get('username').value;
+    let restaurantId = await getBusinessId(username);
+    let menuItems = await getFoodItemsByBusiness(restaurantId.id);
 
     return (
-      <main>
-        <div className="container mx-auto py-8">
-          <div className="max-w-4xl mx-auto">
-            <div className="text-center mb-8">
-              <h1 className="text-3xl font-bold">{restaurantInfo.restaurant?.name}</h1>
-              <p className="text-gray-600">{restaurantInfo.restaurant?.description}</p>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            <div>
-              <h2 className="text-xl font-semibold mb-2">Hours</h2>
-              <p className="text-gray-700">Monday: {hours?.monOpen.toLocaleString([], {hour: '2-digit', minute:'2-digit'})} - {hours?.monClose.toLocaleString([], {hour: '2-digit', minute:'2-digit'})}</p>
-              <p className="text-gray-700">Tuesday: {hours?.tuesOpen.toLocaleString([], {hour: '2-digit', minute:'2-digit'})} - {hours?.tuesClose.toLocaleString([], {hour: '2-digit', minute:'2-digit'})}</p>
-              <p className="text-gray-700">Wednesday: {hours?.wedOpen.toLocaleString([], {hour: '2-digit', minute:'2-digit'})} - {hours?.wedClose.toLocaleString([], {hour: '2-digit', minute:'2-digit'})}</p>
-              <p className="text-gray-700">Thursday: {hours?.thurOpen.toLocaleString([], {hour: '2-digit', minute:'2-digit'})} - {hours?.thurClose.toLocaleString([], {hour: '2-digit', minute:'2-digit'})}</p>
-              <p className="text-gray-700">Friday: {hours?.friOpen.toLocaleString([], {hour: '2-digit', minute:'2-digit'})} - {hours?.friClose.toLocaleString([], {hour: '2-digit', minute:'2-digit'})}</p>
-              <p className="text-gray-700">Saturday: {hours?.satOpen.toLocaleString([], {hour: '2-digit', minute:'2-digit'})} - {hours?.satClose.toLocaleString([], {hour: '2-digit', minute:'2-digit'})}</p>
-              <p className="text-gray-700">Sunday: {hours?.sunOpen.toLocaleString([], {hour: '2-digit', minute:'2-digit'})} - {hours?.sunClose.toLocaleString([], {hour: '2-digit', minute:'2-digit'})}</p>
-            </div>
-              <div>
-                <h2 className="text-xl font-semibold mb-2">Contact Information</h2>
-                <p className="text-gray-700">Name: {contact?.firstName} {contact?.lastName}</p>
-                <p className="text-gray-700">Address: {contact?.buildingNumber} {contact?.street} {contact?.unitNumber}, {contact?.city}, {contact?.state} {contact?.zipCode}</p>
-                <p className="text-gray-700">Phone Number: {contact?.phoneNumber}</p>
-              </div>
-            </div>
-            <div className="mt-8">
-              <h2 className="text-xl font-semibold mb-2">Menu</h2>
-                <div className="bg-white rounded-lg shadow-md p-4">
-                {restaurantInfo.menuItems.map((item, index) => (
-                  value ? (
-                    <ModifiableMenuItem key={index} item={item} />
-                  ) : (
-                    <MenuItem key={index} item={item} />
-                  )
-                ))}
-              </div>
-            </div>
+      <div className="mt-8">
+        <h2 className="text-xl font-semibold mb-2">Menu</h2>
+          <div className="bg-white rounded-lg shadow-md p-4">
+            {menuItems.map((item, index) => (
+              <ModifiableMenuItem key={index} item={item} />
+            ))}
           </div>
-        </div>
-        <div className="fixed bottom-4 right-4">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center">
-            <ShoppingCartIcon className="h-6 w-6 mr-2" />
-            Shopping Cart
-            <div className="absolute top-0 right-0 bg-red-500 rounded-full h-6 w-6 flex items-center justify-center text-white text-xs -mt-2 -mr-2">
-              5
-            </div>
-          </button>
-        </div>
-      </main>
+      </div>
     );
-   } catch (error) {
+  } catch (error) {
     console.error('Error fetching data:', error);
     return (
       <main>
@@ -84,102 +31,19 @@ export default async function Page() {
   }
 }
 
-function MenuItem({ item }) {
-  const [quantity, setQuantity] = useState(0);
-  const inputRef = useRef(null);
-
-  const incrementQuantity = () => {
-    setQuantity(quantity + 1);
-  };
-
-  const decrementQuantity = () => {
-    if (quantity > 0) {
-      setQuantity(quantity - 1);
-    }
-  };
-
-  const handleChange = (e) => {
-    const value = e.target.value;
-    if (value === "" || (Number.isInteger(parseInt(value)) && parseInt(value) >= 0)) {
-      setQuantity(value === "" ? 0 : parseInt(value));
-      inputRef.current.style.width = `${inputRef.current.scrollWidth}px`;
-    }
-  };
-
-  return (
-    <div className="relative flex justify-between items-center bg-gray-200 mb-2 p-2 rounded-lg hover:bg-sky-100 hover:text-blue-600 dark:hover:bg-sky-900 dark:hover:text-blue-400">
-      <div>
-        <p className="text-gray-700 dark:text-gray-300">{item.name}</p>
-        <p className="text-gray-700 dark:text-gray-300">${item.price.toFixed(2)}</p>
-        <p className="text-gray-700 dark:text-gray-300">Available: {item.stock}</p>
-      </div>
-      <div className="flex items-center justify-end">
-        <button className="flex-shrink-0 bg-red-500 hover:bg-red-700 text-white font-bold rounded-full flex items-center justify-center w-8 h-8 text-lg mr-2"
-        onClick={decrementQuantity}>
-          -
-        </button>
-        <input
-          className="text-lg mr-2 w-8 text-center"
-          type="text"
-          value={quantity}
-          onChange={handleChange}
-          ref={inputRef}
-        />
-        <button className="flex-shrink-0 bg-red-500 hover:bg-red-700 text-white font-bold rounded-full flex items-center justify-center w-8 h-8 text-lg mr-5"
-        onClick={incrementQuantity}>
-          +
-        </button>
-      </div>
-    </div>
-  );
-}
-
 function ModifiableMenuItem({ item }) {
-  const [quantity, setQuantity] = useState(0);
-  const inputRef = useRef(null);
-
-  const incrementQuantity = () => {
-    setQuantity(quantity + 1);
-  };
-
-  const decrementQuantity = () => {
-    if (quantity > 0) {
-      setQuantity(quantity - 1);
-    }
-  };
-
-  const handleChange = (e) => {
-    const value = e.target.value;
-    if (value === "" || (Number.isInteger(parseInt(value)) && parseInt(value) >= 0)) {
-      setQuantity(value === "" ? 0 : parseInt(value));
-      inputRef.current.style.width = `${inputRef.current.scrollWidth}px`;
-    }
-  };
-
-  return (
-    <div className="relative flex justify-between items-center bg-gray-200 mb-2 p-2 rounded-lg hover:bg-sky-100 hover:text-blue-600 dark:hover:bg-sky-900 dark:hover:text-blue-400">
-      <div>
-        <p className="text-gray-700 dark:text-gray-300">{item.name}</p>
-        <p className="text-gray-700 dark:text-gray-300">${item.price.toFixed(2)}</p>
-        <p className="text-gray-700 dark:text-gray-300">Available: {item.stock}</p>
-      </div>
-      <div className="flex items-center justify-end">
-        <button className="flex-shrink-0 bg-red-500 hover:bg-red-700 text-white font-bold rounded-full flex items-center justify-center w-8 h-8 text-lg mr-2"
-        onClick={decrementQuantity}>
-          -
+    return (
+      <div className="relative flex justify-between items-center bg-gray-200 mb-2 p-2 rounded-lg hover:bg-sky-100 hover:text-blue-600 dark:hover:bg-sky-900 dark:hover:text-blue-400">
+        <div>
+          <p className="text-gray-700 dark:text-gray-300">{item.name}</p>
+          <p className="text-gray-700 dark:text-gray-300">${item.price.toFixed(2)}</p>
+          <p className="text-gray-700 dark:text-gray-300">Available: {item.stock}</p>
+        </div>
+        <div className="flex items-center justify-end">
+        <button className="bg-red-500 hover:bg-red-700 text-white font-bold flex rounded mr-5 items-center justify-center w-auto h-12 px-4 text-sm">
+            Modify Item
         </button>
-        <input
-          className="text-lg mr-2 w-8 text-center"
-          type="text"
-          value={quantity}
-          onChange={handleChange}
-          ref={inputRef}
-        />
-        <button className="flex-shrink-0 bg-red-500 hover:bg-red-700 text-white font-bold rounded-full flex items-center justify-center w-8 h-8 text-lg mr-5"
-        onClick={incrementQuantity}>
-          +
-        </button>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
