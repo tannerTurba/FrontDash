aef016f881382b4d38c57db7b68820d3ada3d39e
Matthew Moyer <moyer0729@uwlax.edu>
2024-04-14 12:02:02 -0500

    menu

--- frontdash/app/menu/page.tsx
+++ frontdash/app/menu/page.tsx
@@ -92,3 +92,53 @@ export default async function Page() {
     );
   }
 }
+
+function MenuItem({ item }) {
+  const [quantity, setQuantity] = useState(0);
+  const inputRef = useRef(null);
+
+  const incrementQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decrementQuantity = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    if (value === "" || (Number.isInteger(parseInt(value)) && parseInt(value) >= 0)) {
+      setQuantity(value === "" ? 0 : parseInt(value));
+      inputRef.current.style.width = `${inputRef.current.scrollWidth}px`;
+    }
+  };
+
+  return (
+    <div className="relative flex justify-between items-center bg-gray-200 mb-2 p-2 rounded-lg hover:bg-sky-100 hover:text-blue-600 dark:hover:bg-sky-900 dark:hover:text-blue-400">
+      <div>
+        <p className="text-gray-700 dark:text-gray-300">{item.name}</p>
+        <p className="text-gray-700 dark:text-gray-300">${item.price.toFixed(2)}</p>
+        <p className="text-gray-700 dark:text-gray-300">Available: {item.stock}</p>
+      </div>
+      <div className="flex items-center justify-end">
+        <button className="flex-shrink-0 bg-red-500 hover:bg-red-700 text-white font-bold rounded-full flex items-center justify-center w-8 h-8 text-lg mr-2"
+        onClick={decrementQuantity}>
+          -
+        </button>
+        <input
+          className="text-lg mr-2 w-8 text-center"
+          type="text"
+          value={quantity}
+          onChange={handleChange}
+          ref={inputRef}
+        />
+        <button className="flex-shrink-0 bg-red-500 hover:bg-red-700 text-white font-bold rounded-full flex items-center justify-center w-8 h-8 text-lg mr-5"
+        onClick={incrementQuantity}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
